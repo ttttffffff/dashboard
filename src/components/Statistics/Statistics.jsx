26184c04aba56790deeac2b6f7fd9ca28c6ab01a
wyt <295664857@qq.com>
2024-04-24 10:28:43 +0800

    fix: weather png

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,19 +6,18 @@ import StatisticsChart from '../StatisticsChart/StatisticsChart'
 import { TiWeatherCloudy, TiWeatherDownpour, TiWeatherSnow, TiWeatherStormy, TiWeatherSunny, TiWeatherWindy, TiWeatherWindyCloudy } from "react-icons/ti";
 import { IoIosPartlySunny } from "react-icons/io";
 import { getHumidity } from '../../api/weather'
-import { wmoToWeather,getWeatherCategory } from '../../data/weather_code'
+import { wmoToWeather } from '../../data/weather_code'
 
 const LONGITUDE='114.13'
 const LATITUDE='30.68'
 
 
 export default function Statistics() {
-  // const [Icon, setIcon] = useState(TiWeatherDownpour)
   const [weaname, setWeaName] = useState('sunny')
   const [humid,setHumid]=useState('%')
   const [wind,setWind]=useState('')
   const [sunset,setSunset]=useState('')
-  let Icon=TiWeatherDownpour
+
   const formattime=(time)=>{
     const utcTime = new Date(time);
     const chinaTime = new Date(utcTime.getTime() + 8 * 60 * 60 * 1000);
@@ -35,10 +34,35 @@ export default function Statistics() {
       setWind(res.data.current.wind_speed_10m.toString()+" Km/h")
       setSunset(formattime(res.data.daily.sunset[0]))
       setWeaName(wmoToWeather(res.data.current.weather_code.toString()))
-      Icon=getWeatherCategory(weaname)
-      // setIcon(getWeatherCategory(weaname))
     })
   },[])
+  const getWeatherCategory=(weatherName)=> {
+    console.log('111',weatherName)
+    if(weatherName.includes('Rain')){
+        return <TiWeatherDownpour/>
+    }else if(weatherName.includes('Clouds')){
+        return <TiWeatherCloudy/>
+    }    else if(weatherName.includes('Thunderstorm')){
+        return  <TiWeatherStormy/>
+    }
+    else if(weatherName.includes('Snow')){
+        return <TiWeatherSnow/>
+    }
+    else if(weatherName.includes('Showers')){
+        return <BsCloudDrizzleFill/>
+    }
+    else if(weatherName.includes('Drizzle')){
+        return <BsCloudDrizzleFill/>
+    }
+    else if(weatherName.includes('Fog')){
+        return <BsCloudFogFill/>
+    }
+    else{
+        return <TiWeatherSunny/>
+    }
+
+    
+}
 
   return (
     <div className={`${css.container} theme-container`}>
@@ -46,8 +70,7 @@ export default function Statistics() {
       <div className={`${css.cards} grey-container`}>
         <div>
           <div className={css.arrowIcon}>
-              {/* {Icon} */}
-              <Icon/>
+              {getWeatherCategory(weaname)}
           </div>
           <div className={css.card}>
             <span>Weather today</span><span>{weaname}</span>
